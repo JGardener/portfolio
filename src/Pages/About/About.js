5f22db34aa6640c90ba611f0ce45df21f202131c
James Gardener <47278538+JGardener@users.noreply.github.com>
2022-11-14 13:06:18 +0000

    Removed "About" clutter
    Reworded and cleaned up the about section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,56 +5,49 @@ const About = () => {
   return (
     <section className={styles.about_wrapper}>
       <h1 className={styles.about_title}>What have I learned?</h1>
-      <h4>Base</h4>
-      <p>HTML, CSS, JS and TailwindCSS</p>
-      <p>
-        The trifecta is where I began my coding journey, and consistently
-        working on the fundamentals makes working with other technologies that
-        build on these a lot easier. Furthermore, TailwindCSS has made my CSS
-        process much cleaner and more efficient.
-      </p>
-      <br />
-      <h4>Frameworks</h4>
-      <p>
-        React - Whilst I'm focussing on React, with particular focus on Hooks
-        and Redux Toolkit, I am more than willing to upskill in Vue and/or
-        Angular
-      </p>
-      <br />
-      <h4>Beginning to learn</h4>
-      <p>PHP, Laravel, Wordpress</p>
-      <br />
-      <h4>Whilst at work</h4>
-      <p>Up-skilling HTML, CSS, JS and TailwindCSS</p>
-      <p>
-        Optimising websites for deployment; image compression, prefetch/preload,
-        CDN/local files, clean directory structure, GTMetrix et al.
-      </p>
-      <p>
-        Incorporating libraries into work, TinySlider for carousels, FancyBox
-        for modal videos/images, LeafletJS for interactive maps, ChartJS for
-        charting, etc
-      </p>
-      <p>
-        Adobe Photoshop and Adobe XD - Photoshop to resize and make small
-        adjustments to images, and designers would send through their designs to
-        me in XD
-      </p>
-      <br />
-      <h4>What I'm learning right now</h4>
-      <p>React - it's always ongoing!</p>
-      <p>React Hooks</p>
-      <p>Redux Toolkit</p>
-      <p>jQuery</p>
-      <p>PHP & Laravel</p>
-      <p>Any libraries that make any of my projects smoother!</p>
-      <br />
-      <h4>What's my plan?</h4>
-      <p>
-        I'd like to focus on front-end work at the moment, but once I complete
-        my time as a junior, and move into mid-level or senior work, I'd like to
-        expand to full-stack development!
-      </p>
+      <div>
+        <p>
+          The trifecta of HTML, CSS and JS is where I began my coding journey,
+          and consistently working on the fundamentals makes working with other
+          technologies that build on these a lot easier. I firmly believe that
+          consistently working on fundamentals makes me a stronger developer
+          than just rushing into frameworks without knowing what's going on
+          under the hood.
+        </p>
+      </div>
+      <div>
+        <h4>Whilst at Wonderful</h4>
+        <p>Up-skilling HTML, CSS, JS and TailwindCSS</p>
+        <p>
+          Optimising websites for deployment; image compression,
+          prefetch/preload, CDN/local files, clean directory structure, GTMetrix
+          et al.
+        </p>
+        <p>
+          Incorporating libraries into work, TinySlider for carousels, FancyBox
+          for modal videos/images, LeafletJS for interactive maps, ChartJS for
+          charting, etc
+        </p>
+        <p>
+          Adobe Photoshop and Adobe XD - Photoshop to resize and make small
+          adjustments to images, and designers would send through their designs
+          to me in XD.
+        </p>
+      </div>
+
+      <div>
+        <h4>What's my plan?</h4>
+        <p>
+          I'd like to focus on front-end work at the moment, but once I complete
+          my time as a junior, and move into mid-level or senior work, I'd like
+          to expand to full-stack development!
+        </p>
+        <p>
+          I'd like to look into Python and Java, as they're both incredibly
+          popular. Java is a full programming language, and therefore something
+          I'd love to get my teeth into once I move into full-stack work.
+        </p>
+      </div>
     </section>
   );
 };
